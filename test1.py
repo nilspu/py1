08a408d29193a1ad832f22c6fea9ea7f5ac41114
nilspu <nils.p@arcor.de>
2021-09-14 21:31:52 +0200

    newupd

--- test1.py
+++ test1.py
@@ -1,1 +1,7 @@
 print("Hallo Nase3!")
+print(13 - 5 * 2 + 12/6)
+print(7/2 - 5/4)
+u_inch=2.54
+print("5 Inch sind " , (5 * u_inch) , " cm")
+print("20 Inch sind " , (20 * u_inch) , " cm")
+print("92,7 Inch sind " , (92.7 * u_inch) , " cm")
