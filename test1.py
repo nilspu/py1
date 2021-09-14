e72d9c40d5453c58b58874fa583daf1668b8df14
nilspu <nils.p@arcor.de>
2021-09-14 21:34:29 +0200

    upd4

--- test1.py
+++ test1.py
@@ -5,4 +5,4 @@ u_inch=2.54
 print("5 Inch sind " , (5 * u_inch) , " cm")
 print("20 Inch sind " , (20 * u_inch) , " cm")
 print("92,7 Inch sind " , (92.7 * u_inch) , " cm")
-print("Ende")
+print("Ende2")
